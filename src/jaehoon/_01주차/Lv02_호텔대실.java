8a06b4fab83493e1706092b2ead7c54b34f4865b
ljh468 <ljh468@naver.com>
2024-12-08 17:16:45 +0900

    feat: solving programmers problem [Lv02_호텔대실, Lv03_네트워크]

--- src/jaehoon/_01주차/Lv02_호텔대실.java
+++ src/jaehoon/_01주차/Lv02_호텔대실.java
@@ -1,4 +1,4 @@
-package jaehoon;
+package jaehoon._01주차;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/jaehoon/_01주차/Lv03_네트워크.java
+++ src/jaehoon/_01주차/Lv03_네트워크.java
@@ -1,4 +1,4 @@
-package jaehoon;
+package jaehoon._01주차;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/43162
 public class Lv03_네트워크 {
