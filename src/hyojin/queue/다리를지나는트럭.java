b907688ef210c8c029bff073cbe50e89fda44414
hyojin202406 <hyojin202406@gmail.com>
2024-12-09 17:40:24 +0900

    refactor: 주석 추가

--- src/hyojin/queue/다리를지나는트럭.java
+++ src/hyojin/queue/다리를지나는트럭.java
@@ -3,6 +3,8 @@ package hyojin.queue;
 import java.util.LinkedList;
 import java.util.Queue;
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/42583
+
 public class 다리를지나는트럭 {
     public static void main(String[] args) {
         int bridge_length = 2;
