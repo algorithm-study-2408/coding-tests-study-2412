d6b51f59f69788473ec832b8480e1c5fd92fca51
wn1331 <wn1331@gmail.com>
2024-12-11 09:35:24 +0900

    2차 풀이, 성능 개선

--- src/jonghun/week02/베스트앨범.java
+++ src/jonghun/week02/베스트앨범.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 
 /**
- * 1. 가변형 answer 리스트 생성<br/> 2. 장르별 재생 횟수 HashMap<br/> 3. 장르-노래별 번호 및 재생횟수 HashMap(이중해시뱀)<br/> 4.
- * 3번의 내부해시맵 value 내림차순 정렬<br/> 5. key추출 but 2개씩만<br/> 6. answer에 적재 but 장르별재생횟수에 따른 순서로<br/> (리스트
+ * 1. 가변형 answer 리스트 생성<br/> 2. 장르별 재생 횟수 HashMap<br/> 3. 장르-노래별 번호 및 재생횟수 HashMap<`String,int[]><br/> 4.
+ * 3번의 int[] value 내림차순 정렬<br/> 5. key추출 but 2개씩만<br/> 6. answer에 적재 but 장르별재생횟수에 따른 순서로<br/> (리스트
  * 없이 종류*2하면 primitive 배열 될수도 있지만 1개일때 고려)<br/> 7. primitive array 변환<br/>
+ *
  */
 public class 베스트앨범 {
 
@@ -20,47 +21,34 @@ public class 베스트앨범 {
     public static int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
 
-        Map<String, Integer> num = new HashMap<>();
-        Map<String, Map<Integer, Integer>> music = new HashMap<>();
+        Map<String, Integer> genreSum = new HashMap<>();
+        Map<String, List<int[]>> musics = new HashMap<>();  // [id, play count]
 
         // 적재 로직
         for (int i = 0; i < plays.length; i++) {
-            // genres와 plays의 길이가 같으니 맵을 하나씩 채워주자. 단, num에 키가 이미 존재하면 num에는 합산을 하고 music에는 추가적재 해야한다.
-            if (!num.containsKey(genres[i])) {
-                Map<Integer, Integer> temp = new HashMap<>();
-                temp.put(i, plays[i]);
-                music.put(genres[i], temp);
-                num.put(genres[i], plays[i]);
-            } else {
-                // num에 키가 이미 존재할 때 처리
-                music.get(genres[i]).put(i, plays[i]);
-                num.put(genres[i], num.get(genres[i]) + plays[i]);
-            }
+            // merge 메서드 굿..
+            genreSum.merge(genres[i],plays[i],Integer::sum);
+            // computeIfAbsent 활용. genres[i]가 존재하지 않으면 배열생성하고 add, 존재하면 그냥 add
+            musics.computeIfAbsent(genres[i], key->new ArrayList<>()).add(new int[]{i,plays[i]});
         }
 
-        // num 정렬 로직
-        List<String> keySet = new ArrayList<>(num.keySet());
-        keySet.sort((Comparator.comparingInt(num::get)).reversed());
+        // genreSum 내림차순 정렬 로직
+        List<String> keySet = new ArrayList<>(genreSum.keySet());
+        keySet.sort((a, b) -> genreSum.get(b) - genreSum.get(a));
 
-        // music 해시맵 정렬 및 answer 적재 로직
+        // 각 장르별로 노래 선택 2개
         for (String key : keySet) {
-            // temp 꺼내옴
-            Map<Integer, Integer> temp = music.get(key);
-            // index 저장되어있는 temp의 key들 나열함
-            List<Integer> keys = new ArrayList<>(temp.keySet());
-            // temp의 value대로 keys 정렬
-            keys.sort((Comparator.comparingInt(temp::get).reversed()));
+            // 노래 꺼내옴
+            List<int[]> genreSongs = musics.get(key);
+            // 재생 횟수로 내림차순 정렬
+            genreSongs.sort((a, b) -> b[1] - a[1]);
 
-            // 내림차순 정렬된 상태에서 두개까지만 적재해야 되니까 여기서 get(0)하고 조건문에서 get(1)
-            answer.add(keys.get(0));
-            if(keys.size()>1){
-                // 내림차순 정렬된 상태에서 두개까지만 적재해야 되니까 get(1)
-                answer.add(keys.get(1));
+            answer.add(genreSongs.get(0)[0]);  // 첫 번째 곡
+            if(genreSongs.size() > 1) {        // 두 번째 곡이 있다면
+                answer.add(genreSongs.get(1)[0]);
             }
-
         }
-
         // primitive array로 변환 return
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
