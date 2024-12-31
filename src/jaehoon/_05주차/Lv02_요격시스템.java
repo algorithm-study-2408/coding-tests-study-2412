c905736f3d50118d0b5530ab3a46b0a6e2358393
ljh468 <ljh468@naver.com>
2025-01-01 08:09:50 +0900

    feat: solving programmers problem[Lv02_요격시스템, Lv02_이모티콘_할인행사]

--- src/jaehoon/_05주차/Lv02_요격시스템.java
+++ src/jaehoon/_05주차/Lv02_요격시스템.java
@@ -15,9 +15,10 @@ public class Lv02_요격시스템 {
     // 1. end를 기준으로 오름차순 정렬
     Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
 
-    // 2. 첫 번째로 만들어지는 요격 시스템
+    // 2. 요격 미사일 개수, 요격 지점 초기화
     int end = -1;
     int answer = 0;
+
     // 3. 정렬된 폭격 미사일 하나씩 ㅅ접근하기
     for(int[] tar : targets){
       // 다음 요격미사일의 시작 지점이 요격의 end지점보다 크거나 같으면 (겹치지 않으면)
